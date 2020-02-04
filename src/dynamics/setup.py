a1b4c732f078dad133b7771ced7171a4f71b7d2d
Michael Rencheck <rencheck.1@live.com>
2020-02-03 21:37:55 -0600

    Updated CMake and package files.

---
+++ src/dynamics/setup.py
@@ -0,0 +1,14 @@
+# Python setup file used for installing a python catkin package.
+# See http://docs.ros.org/melodic/api/catkin/html/user_guide/setup_dot_py.html
+# For catkin to use this we have called python_catkin_setup() in CMakeLists.txt
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages = ['dynamics'],
+    # scripts = ['scripts/script1.py', 'scripts/script2.py'],
+    package_dir={'' : 'src'}
+)
+
+
+setup(**d)
