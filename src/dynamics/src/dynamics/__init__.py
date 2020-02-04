442016b2a74323914237cf304e7c03352c8f846d
Michael Rencheck <rencheck.1@live.com>
2020-02-03 22:43:45 -0600

    Successfully publishing wheel positions

--- src/dynamics/src/dynamics/__init__.py
+++ src/dynamics/src/dynamics/__init__.py
@@ -1,6 +1,6 @@
 """ Make the contents of this library easily accessable to package """
 
 from .simulation_functions import(
-    se3ToVec
-    TransInv
+    se3ToVec,
+    TransInv,
     integrate)
--- src/dynamics/src/dynamics/simulation_functions.py
+++ src/dynamics/src/dynamics/simulation_functions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """
     These are all functions that support computing the Legrangian Dynamics.
