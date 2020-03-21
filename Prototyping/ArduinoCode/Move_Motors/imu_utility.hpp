c066273407bbba8d8657ae6747c0f3f814e8e92a
Michael Rencheck <rencheck.1@live.com>
2020-03-20 23:05:21 -0500

    repo clean up

--- Prototyping/ArduinoCode/Move_Motors/imu_utility.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef IMU_UTILITY_HG
-#define IMU_UTILITY_HG
-
-void displaySensorStatus(Adafruit_BNO055 &bno);
-void displaySensorOffsets(const adafruit_bno055_offsets_t &calibData);
-void loadCalibaration(Adafruit_BNO055 &bno);
-
-#endif
--- Prototyping/ArduinoCode/Move_Motors/motor_driver_utility.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MOTOR_DRIVER_UTILITY_HG
-#define MOTOR_DRIVER_UTILITY_HG
-
-void moveMotorFWD(float command);
-void moveMotorBKD(float command);
-void commandMotor(float command);
-
-
-#endif
